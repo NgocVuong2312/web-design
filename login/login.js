868f9ef4b83e1432bf189ade7b6e4c31257ab464
NgocVuong2312 <11236236@st.neu.edu.vn>
2025-05-27 17:17:59 +0700

    first commit

---
+++ login/login.js
@@ -0,0 +1,21 @@
+function Login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username && password) {
+        const storedData = localStorage.getItem('userdata');
+        if (storedData) {
+            const userData = JSON.parse(storedData);
+            if (userData.username === username && userData.password === password) {
+                alert('Login successful!');
+                return location.href = '../../time manager/home/home.html';
+            } else {
+                alert('Invalid username or password.');
+            }
+        } else {
+            alert('No user data found. Please register first.');
+        }
+    } else {
+    alert('Please enter both username and password.');
+    }
+}
---
+++ signup/register.js
@@ -0,0 +1,27 @@
+function Register() {
+    const username= document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    if (username === "" || password === "" || email === "" || confirmPassword === "") {
+        alert("Please fill in all fields.");
+        return false;
+    }
+    else{
+        if (password !== confirmPassword) {
+            alert("Passwords do not match.");
+            return;
+        }
+        else {
+            const data = {
+                username: username,
+                password: password,
+                email: email
+            }
+            localStorage.setItem("userdata", JSON.stringify(data));
+            alert("Registration successful!");
+            return location.href='../../time manager/login/login.html';
+        }
+    }
+    
+}
